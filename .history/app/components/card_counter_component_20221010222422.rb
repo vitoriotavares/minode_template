13f0ba06affb6423d0aab87f1dd5afcc76f8f2c1
Vitorio <norbdus@gmail.com>
2022-10-11 07:11:40 -0300

    componentizando

---
+++ .history/app/components/card_counter_component_20221010222422.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CardCounterComponent < ViewComponent::Base
+  def initialize(value:, subtitle:, icon:, percent:)
+    @value = value
+    @subtitle = subtitle
+    @icon = icon
+    @percent = percent
+  end
+end
